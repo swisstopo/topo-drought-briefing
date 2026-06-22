37406db24a130c2801b9e7df5357134cb67ec29e
ben-mey <67687123+ben-mey@users.noreply.github.com>
2026-06-22 15:18:00 +0200

    Update hydro_lines formatting for discharge values
    Added hardcoded Unit for discharge values [m3/s]

--- app.py
+++ app.py
@@ -274,8 +274,8 @@ elif view_tab == "regions":
                 hydro_lines.append(
                     f"<b>{header_text}</b><br/>"
                     f"<span style='opacity: 0.8; font-size:13px; line-height: 1.3;'>"
-                    f"{abfluss_label}: {val_str}<br/>"
-                    f"T1: {t1_str} | Min: {min_str}"
+                    f"{abfluss_label}: {val_str} [m&sup3;/s]<br/>"
+                    f"T1: {t1_str} [m&sup3;/s] | Min: {min_str} [m&sup3;/s]"
                     f"</span>"
                 )
             situation = "<br/><br/>".join(hydro_lines)
